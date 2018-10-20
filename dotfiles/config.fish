3d8854b842a67c5b9028037e69c1af8ccb978f41
Kossak <kossak.git@gmail.com>
2018-10-20 14:19:45 +0200

    clear + reorder things

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -4,24 +4,6 @@ if test -d "$dir_java"; and not set -q JAVA_HOME
     set -x JAVA_HOME "$dir_java"
 end
 
-# setup powerline:
-begin
-    if type -q powerline-daemon
-        powerline-daemon -q
-    end
-    set -l dirs \
-        '/home/kossak/.pyenv/versions/3.6.5/lib/python3.6/site-packages/powerline/bindings/fish' \
-        '/home/kossak/anaconda3/lib/python3.6/site-packages/powerline/bindings/fish' \
-        '/home/kossak/.pyenv/versions/miniconda3-4.3.30/lib/python3.6/site-packages/powerline/bindings/fish'
-    for dir in $dirs
-        if test -d "$dir"
-            set fish_function_path $fish_function_path "$dir"
-            powerline-setup
-            break
-        end
-    end
-end
-
 # ssh-agent
 if test -z "$SSH_ENV"
     set -xg SSH_ENV $HOME/.ssh/environment
@@ -30,16 +12,6 @@ end
 if not __ssh_is_agent_started
     __ssh_agent_start
 end
-
-# pipenv completions
-# should be this, but it's slow:
-# eval (pipenv --completion)
-# complete --command pipenv --arguments "(env _PIPENV_COMPLETE=complete-fish COMMANDLINE=(commandline -cp) pipenv)" -f
-
-# pyenv v2: - put it at the end (it modifies paths)
-# set -gx PYENV_ROOT '$HOME/.pyenv'
-# set -gx PATH '$PYENV_ROOT/bin:$PATH'
-# status --is-interactive; and source (pyenv init -|psub)
 
 # pyenv
 set -l pyenv_root "$HOME/.pyenv"
@@ -55,6 +27,25 @@ if test -d "$pyenv_root"
     # auto complete:
     status --is-interactive; and . (pyenv init -|psub)
     status --is-interactive; and . (pyenv virtualenv-init -|psub)
+end
+
+# powerline:
+begin
+    if type -q powerline-daemon
+        powerline-daemon -q
+    end
+    set -l dirs \
+        '/home/kossak/.asdf/installs/python/3.6.6/lib/python3.6/site-packages/powerline/bindings/fish' \
+        '/home/kossak/.pyenv/versions/3.6.5/lib/python3.6/site-packages/powerline/bindings/fish' \
+        '/home/kossak/anaconda3/lib/python3.6/site-packages/powerline/bindings/fish' \
+        '/home/kossak/.pyenv/versions/miniconda3-4.3.30/lib/python3.6/site-packages/powerline/bindings/fish'
+    for dir in $dirs
+        if test -d "$dir"
+            set fish_function_path $fish_function_path "$dir"
+            powerline-setup
+            break
+        end
+    end
 end
 
 # asdf:
