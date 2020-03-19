5c3ac57fe5d27494c44b22eff0eb14a330e221e9
Kossak <kossak.git@gmail.com>
2020-03-19 12:31:15 +0100

    fix pasting in ranger

--- fish_functions/ranger_cd.fish
+++ fish_functions/ranger_cd.fish
@@ -2,7 +2,9 @@
 function ranger_cd
 	set -l tempfile '/tmp/chosendir'
 
+    __fish_disable_bracketed_paste
     ranger --choosedir $tempfile (pwd)
+    __fish_enable_bracketed_paste
     if [ -f "$tempfile" ]; and [ (cat -- $tempfile) != (echo -n (pwd)) ]
         cd (cat $tempfile)
     end
